f41275236873d70319d30bf0d3fac09f17efaddb
AngC1998 <runningnerdtroll@gmail.com>
2018-04-16 23:39:48 -0500

    Add files via upload
    Starter code for navigation of the robot. Probably not where we want to be at, but provides a good starting point for the navigation part of the robot (and maybe if we want to do navigation in JavaScript, a good inspiration to look into), Could have elaborated more on the code such as navigation to the rooms. Please contact Angela if you have any suggestions on how to further elaborate the code to meet the primary goals.

---
+++ navigation.c
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+typedef actionlib::SimpleActionClient<move_base_msg::MoveBaseAction> MoveBaseClient;
+
+//credits to ROS.org Robot Navigation C++ tutorial 
+//not exactly functional but good starting point especially when 
+//navigation plays a crucial role in our project. 
+int main(int argc, char** argv) {
+    //activating action client
+    MoveBaseClient actionClient("move_base", true);
+    
+    move_base_msgs::MoveBaseGoal goal;
+    
+    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.stamp = ros::Time::now();
+    
+    //creating goal to move 1 meter forward
+    goal.target_pose.pose.position.x = 1.0;
+    goal.target_pose.pose.orientation.w = 1.0; 
+    
+    ROS_INFO("Sending goal");
+    actionClient.sendGoal(goal);
+    
+    actionClient.waitForResult();
+    
+    //checking if operation was success
+    if(actionClient.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+        ROS_INFO("SUCCESS");
+    else
+        ROS_INFO("FAILED");
+}
+//will elaborate more on this code as the main purpose of this navigation 
+//is to have to robot lead to the events happening around the GDC
+//will have to add code that has the robot navigate through a plan to get to
+//the event and robot stopping by each room for the event. 
