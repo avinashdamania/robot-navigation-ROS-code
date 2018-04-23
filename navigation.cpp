8751878a6b54be5e40c66a44886d4a86b8529a2d
AngC1998 <runningnerdtroll@gmail.com>
2018-04-23 14:55:26 -0500

    navigation.cpp
    Updated navigation to include visiting doors.

--- navigation.cpp
+++ navigation.cpp
@@ -18,51 +18,68 @@ int main(int argc, char** argv) {
 
     ros::NodeHandle privateNode("~");
     string location;
-    privateNode.param<string>("location", location, "13_414b"); 
+//  privateNode.param<string>("location", location, "13_414b"); 
 
-    ROS_INFO_STREAM("going to " << location);
+    std::vector<string> doors;
+    
+    doors.push_back("d2_210"); 
+    doors.push_back("d4_304"); 
+    doors.push_back("d5_302");
+    doors.push_back("d6_302"); 
+    
+    int currentDoor = 0; 
 
     Client client("action_executor/execute_plan", true);
     client.waitForServer(); 
-    
-    plan_execution:ExecutePlanGoal goal;
-    
-    plan_execution::AspRule rule;
-    plan_execution::AspFluent fluent;
-    fluent.name = "not at";
 
-    fluent.variables.push_back(location); 
+    bool fromAtoB = true; 
     
-    rule.body.push_back(fluent);
-    goal.aspGoal.push_back(rule); 
-    
-    ROS_INFO("Sending goal");
-    actionClient.sendGoal(goal);
+    while(ros::ok()) {
+	location = doors.at(current_door); 
+	current_door++;
+	if(current_door >= (int) doors.size())
+	    current_door = 0; 
 
-    ros::Rate wait_rate(10);
-    while(ros::ok() && !client.getState().isDone()) {
-	wait_rate.sleep();
-    }
+	ROS_INFO_STREAM("going to " << location);
+	plan_execution:ExecutePlanGoal goal;
     
-    if(!client.getState().isDone()) {
-	ROS_INFO("Canceling goal");
-	client.cancelGoal();
-	for(int i = 0; !client.getState.isDone() && i < 50; ++i) 
+    	plan_execution::AspRule rule;
+    	plan_execution::AspFluent fluent;
+    	fluent.name = "not facing";
+
+    	fluent.variables.push_back(location); 
+    
+    	rule.body.push_back(fluent);
+    	goal.aspGoal.push_back(rule); 
+    
+    	ROS_INFO("Sending goal");
+    	actionClient.sendGoal(goal);
+
+    	ros::Rate wait_rate(10);
+    	while(ros::ok() && !client.getState().isDone()) {
 	    wait_rate.sleep();
-    }
+    	}
     
-    //checking if operation was success
-    if(client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
-	ROS_INFO("Aborted");
+    	if(!client.getState().isDone()) {
+	    ROS_INFO("Canceling goal");
+	    client.cancelGoal();
+	    for(int i = 0; !client.getState.isDone() && i < 50; ++i) 
+	    	wait_rate.sleep();
+    	}
+
+	//checking if operation was success
+    	if(client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
+	    ROS_INFO("Aborted");
+    	}
+    	else if(client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
+	    ROS_INFO("Preempted");
+        }
+    	else if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+	    ROS_INFO("Succeeded!");
+    	}
+    	else
+	    ROS_INFO("Terminated"); 
     }
-    else if(client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
-	ROS_INFO("Preempted");
-    }
-    else if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-	ROS_INFO("Succeeded!");
-    }
-    else
-	ROS_INFO("Terminated"); 
 
     return 0; 
 }
