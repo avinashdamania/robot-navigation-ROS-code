a3ad0d1463c9e964ca36a0224c083469f46b8027
avinashdamania <avinashdamania@hotmail.com>
2018-05-07 16:35:46 -0500

    final commit

--- catkin_ws/src/hw5/src/rso.cpp
+++ catkin_ws/src/hw5/src/rso.cpp
@@ -1,3 +1,4 @@
+
 #include <ros/ros.h>
 #include "plan_execution/ExecutePlanAction.h"
 #include <actionlib/client/simple_action_client.h>
@@ -8,19 +9,25 @@ typedef actionlib::SimpleActionClient<plan_execution::ExecutePlanAction> Client;
 
 using namespace std;
 
-//std::queue<string> doors;
 ros::Publisher chatter_pub;
 string location;
+int stopBool;
+
+
 
 
 void navCallback(const std_msgs::String::ConstPtr& loc) {
-    
+
+    //TLDR: By creating a bad location destination, the robot stops.
+    location = loc->data;
+
     Client client("/action_executor/execute_plan", true);
     client.waitForServer();
-    location = loc->data;
-    //string location = doors.front();
-    //doors.pop();
-    
+    if (location.compare("eeeee") == 0) {
+        stopBool = 1;
+    }
+
+
     ROS_INFO_STREAM("going to " << location);
     
     plan_execution::ExecutePlanGoal goal;
@@ -35,35 +42,30 @@ void navCallback(const std_msgs::String::ConstPtr& loc) {
     goal.aspGoal.push_back(rule);
     
     ROS_INFO("sending goal");
-    client.sendGoalAndWait(goal);
+    //maybe use clientgoalhandle?
+    client.sendGoal(goal);
     
     
     ros::Rate wait_rate(10);
     while (ros::ok() && !client.getState().isDone()) {
         
         
-        
+        if (stopBool) {
+
+        ROS_INFO("canceling goal");
+        client.sendGoal(goal);
+        client.cancelGoal();
+        stopBool = 0;
+    }
         
         wait_rate.sleep();
-    }
-    
-    
-    if(!client.getState().isDone()) {
-        ROS_INFO("canceling goal");
-        client.cancelGoal();
-        
-        
-        for (int i = 0; i < 50 && !client.getState().isDone();i++) {
-            wait_rate.sleep();
-        }
-    }
-    
-    
-    
+    }      
+
     if (client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
         ROS_INFO("Aborted");
     } else if (client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
         ROS_INFO("Preempted");
+        return;
     } else if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
         ROS_INFO("Succeeded!");
         std_msgs::String msg;
@@ -78,30 +80,31 @@ void navCallback(const std_msgs::String::ConstPtr& loc) {
         ROS_INFO("Terminated");
     }
     
-    
-    
-    
 }
 
-
+void stopNavCallback(const std_msgs::String::ConstPtr& loc) {
+    ROS_INFO("boolean changed we good ");
+    stopBool = 1;
+}
 
 
 int main(int argc, char**argv) {
     ros::init(argc, argv, "door_navigation");
     ros::NodeHandle n;
-    
-    
-    
+    stopBool = 0;
+        
     
     //doors.push("d3_424");
     
     chatter_pub = n.advertise<std_msgs::String>("chatter_pub", 1000);
-    
     ros::Subscriber sub = n.subscribe("roomNumber", 100, navCallback);
+    ros::Subscriber sub2 = n.subscribe("stopNav", 100, stopNavCallback);
     
-    
-    
-    ros::spin();
+    // ros::spin();
+    //ASYNC SPINNER IS GOD
+    ros::AsyncSpinner spinner(4);
+    spinner.start();
+    ros::waitForShutdown();
     
     return 0;
 }
